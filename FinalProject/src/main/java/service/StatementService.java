df8c9689ad08c993e91764e8f701f97178ddb618
Igor <igor.boikov.ua@gmail.com>
2020-11-23 13:51:24 +0200

    add logger configuration

--- FinalProject/src/main/java/service/StatementService.java
+++ FinalProject/src/main/java/service/StatementService.java
@@ -2,6 +2,8 @@ package service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +13,18 @@ import domain.Statement;
 @Service
 public class StatementService {
 
+	private Logger logger = LoggerFactory.getLogger(StatementService.class);
+	
 	@Autowired
 	private StatementRepository statementRepository;
 
 	public void save(Statement statement) {
+		logger.info("Save statement {} - " + statement);
 		statementRepository.save(statement);
 	}
 
 	public List<Statement> findAllStatements() {
+		logger.info("Find all registered statement");
 		return statementRepository.findAll();
 	}
 }
